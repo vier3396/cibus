bb4516c3d98b21e88dde27fffdbd424537f5e1dc
vier3396 <viktor.eriksson.3396@user.uu.se>
2020-06-05 14:43:04 +0200

    fixed loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
             elevation: 0.0,
           ),
         ),
-        initialRoute: '/',
+        initialRoute: '/loginPage',
         routes: {
           '/': (context) => LoadingScreen(),
           //'/add': (context) => AddRecipe(),
